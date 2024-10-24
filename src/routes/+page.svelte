78fbd20b0e87401a1b524a0ea1d2b5ffd36b90b7
MilesHinch <miles.hinchliffe@levellingup.gov.uk>
2024-10-24 16:39:23 +0000

    added dropdown component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import { csvParse } from 'd3-dsv';
+	import data from './data.js';
+	import Dropdown from './Dropdown.svelte';
+	let parsedData = csvParse(data);
+
+	let onsCodes = [...new Set(parsedData.map((el) => el['Local authority code']))];
+
+	let laList = onsCodes.map((code) => ({
+		id: code,
+		label: parsedData.find((el) => el['Local authority code'] == code)['Local authority name']
+	}));
+
+	// goal = drop down menu where user can select LA
+
+	// filter out the data from the big dataset
+</script>
+
+<Dropdown values={laList} />
---
+++ src/routes/Dropdown.svelte
@@ -0,0 +1,80 @@
+<script>
+    import { Dropdown } from "@onsvisual/svelte-components";
+   
+      
+    export let values;
+   
+    // Bind the <select> value to a variable (reactive)
+    let value = {};
+   
+    // Define your own function on change
+    const handleChange = (e) => console.log(e);
+  </script>
+   
+   
+  <div class="theme-wrapper">
+  <Dropdown options={values} bind:value />
+  {#if value.id}
+  {value.id}
+  {/if}
+    </div>
+   
+  <style>
+      @import url("https://cdn.ons.gov.uk/sdc/design-system/67.0.5/css/main.css");
+  @import url("https://cdn.ons.gov.uk/dp-design-system/cc82c54/css/main.css");
+   
+  /* Overrides */
+  .ons-page__content {
+    padding-bottom: 0;
+  }
+   
+  /* Theme settings */
+  html,
+  body {
+    --text: #222;
+    --muted: #777;
+    --hinted: #e2e2e3;
+    --pale: #f0f0f0;
+    --background: #fff;
+    --link: #206095;
+    --link-hover: #003c57;
+    background: var(--background, #fff);
+    height: auto;
+  }
+  .theme-wrapper {
+    color: var(--text);
+    background: var(--background);
+    --ons-color-text: var(--text);
+    --ons-color-text-light: var(--muted);
+    --ons-color-borders: var(--muted);
+      padding: 20px;
+  }
+  a {
+    color: var(--link, --ons-color-text-link);
+  }
+  a:hover {
+    text-decoration: underline solid var(--link-hover, --ons-color-text-link-hover) 2px;
+  }
+  a:hover {
+    color: var(--link-hover, --ons-color-text-link-hover);
+  }
+  .form-label-bold {
+    color: var(--text, #222);
+  }
+  .form-control {
+    background: none;
+    border-color: var(--text, #222);
+    color: var(--text, #222);
+  }
+   
+  /* Additional global styles */
+  .ons-spacer {
+    display: block;
+    height: 40px;
+    margin: 0;
+    padding: 0;
+  }
+  .ons-font-weight__normal {
+    font-weight: normal;
+  }
+  </style>
